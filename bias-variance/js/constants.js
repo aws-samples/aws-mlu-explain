9c8a973364b4c8ef112e31280310af7aa5ff6def
Jared Wilber <wilberjw@amazon.com>
2021-11-16 13:25:59 -0800

    site css

--- bias-variance/js/constants.js
+++
@@ -1,7 +0,0 @@
-const trainColorDot = "#94CAE0";
-const testColorDot = "#E57873";
-const testColorBar = testColorDot;
-const trainColorBar = trainColorDot;
-const lineColor = "green";
-
-export { testColorBar, trainColorBar, trainColorDot, testColorDot, lineColor };
