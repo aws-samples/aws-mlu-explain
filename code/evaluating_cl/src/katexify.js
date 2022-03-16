bb492c4e563520033b970bc4e978a13ccf6f7400
Jared Wilber <wilberjw@amazon.com>
2022-03-16 14:13:09 -0700

    removing old dir

--- code/evaluating_cl/src/katexify.js
+++
@@ -1,9 +0,0 @@
-import katex from "katex";
-
-export default function katexify(math, displayMode = false) {
-  const options = {
-    displayMode: displayMode,
-    throwOnError: false,
-  };
-  return katex.renderToString(math, options);
-}
