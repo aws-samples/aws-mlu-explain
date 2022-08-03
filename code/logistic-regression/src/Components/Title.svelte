76e4dfb5df76ba9560daebdd973e89b0f4bc8017
erinbugbee <erin_bugbee@icloud.com>
2022-08-03 11:09:00 -0700

    Update date

--- code/logistic-regression/src/Components/Title.svelte
+++ code/logistic-regression/src/Components/Title.svelte
@@ -3,7 +3,7 @@
   <h1 class="intro-sub">Regression for classification.</h1>
   <h3 id="intro__date">
     <a href="https://erinbugbee.com/">Erin Bugbee</a> &
-    <a href="https://twitter.com/jdwlbr">Jared Wilber</a>, July 2022
+    <a href="https://twitter.com/jdwlbr">Jared Wilber</a>, August 2022
   </h3>
 </section>
 
