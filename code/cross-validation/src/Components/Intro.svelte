e2e0d912330d1d60508198cced3b3986fb558046
Jared Wilber <wilberjw@amazon.com>
2022-09-26 10:16:34 -0700

    update intro text

--- code/cross-validation/src/Components/Intro.svelte
+++ code/cross-validation/src/Components/Intro.svelte
@@ -3,15 +3,18 @@
 
 <section>
   <p class="body-text">
-    Often in machine learning we want to compare and evaluate models without
-    having to wait for new data. Of course, we could just evaluate predictions
-    on the same data that we used to fit our model’s parameters, but this will
-    give unreliable assessments of our model's ability to generalize. Thus, we’d
-    like to find a way to assess the generalization capabilities of our model
-    without waiting for new data. This article discusses one of the most common
-    approaches for this task: *cross-validation*. We'll first discuss the
-    approach we previously learned, introduce some new approaches, and discuss
-    what values of k are commonly selected and why.
+    Often in machine learning we want to estimate the performance of our models
+    before putting them into production. Of course, we could try evaluating our
+    model's predictions on the same data that we used to fit our model’s
+    parameters, but this will give unreliable assessments of our model's ability
+    to generalize to unseen data. Thus, we’d like to find a way to assess the
+    generalization capabilities of our model without having to wait for new
+    data. This article discusses one of the most common approaches for this
+    task:
+    <span class="bold">cross-validation</span>. We'll first discuss the
+    Validation Set approach we learned in a previous article, describe how Cross
+    Validation extends that approach, and discuss some concerns around using
+    Cross Validation.
   </p>
 </section>
 
