cc9ea37e5b1fe9966deb4ec0b9103372e9f2f761
Jared Wilber <wilberjw@amazon.com>
2022-11-07 12:59:18 -0800

    clean up intro text

--- code/cross-validation/src/Components/Intro.svelte
+++ code/cross-validation/src/Components/Intro.svelte
@@ -4,20 +4,23 @@
 
 <section>
   <p class="body-text">
-    Often in machine learning we want to estimate the performance of our models
-    before putting them into production. Of course, we could try evaluating our
-    model's predictions on the same data that we used to fit our model's
-    parameters, but this will give unreliable assessments of our model's ability
-    to generalize to unseen data. Thus, we'd like to find a way to assess the
-    generalization capabilities of our model without having to wait for new
-    data. This article discusses one of the most common approaches for this
+    In machine learning we need to estimate the performance of a model before we
+    put it into production. While we could just evaluate our model's performance
+    on the same data that we used to fit its parameters, doing so will give us
+    unreliable assessments of our model's ability to generalize to unseen data.
+    Because obtaining new data may be difficult, we'd like to find a way to
+    assess the generalization capabilities of a model without having to wait for
+    new data. This article discusses one of the most common approaches for this
     task:
     <span class="bold">K-Fold Cross-Validation</span>. We'll first discuss the
-    Validation Set approach we learned in the 
-    <a href="https://mlu-explain.github.io/train-test-validation">Train,
-      Test, and Validation Sets article</a>. Then we will describe how K-Fold 
-    Cross-Validation extends that approach, and discuss some concerns around 
-    selecting the values of {@html katexify(`k`, false)}.
+    Validation Set approach we learned in the
+    <a href="https://mlu-explain.github.io/train-test-validation"
+      >Train, Test, and Validation Sets article</a
+    >. Then we will describe how K-Fold Cross-Validation extends that approach,
+    and discuss some concerns around selecting the values of {@html katexify(
+      `k`,
+      false
+    )}.
   </p>
 </section>
 
