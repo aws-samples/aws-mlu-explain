4f74a7ab461787c0e9125d5be2a79b34180a6038
Mia <mimayer@amazon.com>
2023-03-17 18:26:52 -0700

    modified conclusion

--- code/equality-of-odds/src/Components/Conclusion.svelte
+++ code/equality-of-odds/src/Components/Conclusion.svelte
@@ -7,13 +7,15 @@
   <p class="body-header">The End</p>
   <p class="body-text">
     While machine learning algorithms have the potential to revolutionize
-    decision-making, we must be mindful of the biases they may perpetuate.
-    Equality of Odds (EO) offers a promising approach to mitigating bias and
-    promoting diversity in machine learning models. By promoting diverse
-    outcomes, EO can help build fairer and more equitable machine learning
-    systems in areas such as hiring, lending, and criminal justice. However, we
-    must carefully consider the context and potential trade-offs between
-    competing objectives when implementing any approach to bias mitigation.
+    decision-making, we have to ensure that a fairness criteria is used
+    for measuring any potential bias in addition to general Machine Learning metrics.
+    Depending on the outcome of the bias evaluation we 
+    should include bias mitigation. Equality of Odds (EO) offers a promising
+    approach to mitigate bias and is a method that can be used in different ways
+    (and even during post-processing with access only to the predictions).
+    However, before using EO for evaluation or bias mitigation, we should
+    carefully consider the context and potential trade-offs between competing
+    objectives.
   </p>
 </section>
 
