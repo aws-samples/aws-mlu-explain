48c089f9c3b020cc53c3abfddebb87f840e607a0
Mia <mimayer@amazon.com>
2023-04-07 14:04:56 -0700

    updated phrasing

--- code/equality-of-odds/src/Components/Intro.svelte
+++ code/equality-of-odds/src/Components/Intro.svelte
@@ -6,12 +6,13 @@
 <section>
   <p class="body-text">
     Machine Learning models learn to make predictions by looking at data with
-    the help of algorithms, both of which can be biased against different groups
-    of people. Biased machine learning can harm, discriminate and negatively
-    stereotype against underrepresented or (historically and otherwise)
-    disfavored groups. Therefor, it is crucial to <span class="highlight"
-      >evaluate and control model predictions not only for general machine
-      learning performance but also for fairness</span
+    the help of algorithms, both of which can potentially be biased against
+    different groups of people. Unwanted bias in machine learning can
+    inadvertently harm, and negatively stereotype against underrepresented or
+    (historically and otherwise) disfavored groups. Therefor, it is crucial to <span
+      class="highlight"
+      >evaluate and control data and model predictions not only for general machine
+      learning performance but also for bias</span
     >.
   </p>
 </section>
