829ea0f6f3034bd8342d1f43f4265af03cdbf92a
Jared Wilber <wilberjw@amazon.com>
2023-06-12 13:00:46 -0700

    remove comps

--- code/neural-networks/comps/src/katexify.js
+++
@@ -1,1 +0,0 @@
-import r from"../node_modules/katex/dist/katex.js";function o(o,t=!1){const e={displayMode:t,throwOnError:!1};return r.renderToString(o,e)}export{o as default};
--- code/neural-networks/comps/src/main.js
+++
@@ -1,1 +0,0 @@
-import t from"./App.svelte.js";const e=new t({target:document.body});export{e as default};
